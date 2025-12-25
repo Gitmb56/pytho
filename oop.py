7064b4f4e604b0e5433094609b3bfdda67c0285b
Gitmb56 <digib453@gmail.com>
2025-12-25 08:22:25 +0530

    add new code rectice oop

--- oop.py
+++ oop.py
@@ -1,13 +1,16 @@
 # Prectice Object Orianted  Programme...........
 class Lalita:
-    def __init__(self,name,age,status):
-		self.name=name
-		self.age=age
-		self.status=status
-    def behaviour(self):
-            return f"very sensitive girl"
-    def work(self):
-            return f"housewife"
+    def __init__(self, name, age, status):
+        self.name = name
+        self.age = age
+        self.status = status
 
-my_wife = Lalita("Lalita",22,married)
-my_wife.behaviour()
+    def behaviour(self):
+        return "very sensitive girl"
+
+    def work(self):
+        return "housewife"
+
+
+my_wife = Lalita("Lalita", 22, "married")
+print(my_wife.behaviour())
