31f1c36e360867c4b71bf28a3aaf4f28e76b0e62
Gitmb56 <digib453@gmail.com>
2025-12-25 08:01:29 +0530

    add new oops code

--- oop.py
+++ oop.py
@@ -1,17 +1,13 @@
 # Prectice Object Orianted  Programme...........
-class Cat:
-	def __init__(self,name,age,color):
-		self.name = name
-		self.age = age 
-		self.color = color
-	def sound(self):
-		return f"my name is {self.name} (Meow)..age {self.age} my color {self.color}"
-    def all_fun_prec(self):
-		for i in range(0,11):
-			print(i)
-d = Cat("Pushe",2,"gray shadow")
+class Lalita:
+    def __init__(self,name,age,status):
+		self.name=name
+		self.age=age
+		self.status=status
+    def behaviour(self):
+            return f"very sensitive girl"
+    def work(self):
+            return f"housewife"
 
-print(d.sound())
-#print(d.__dict__)
-#print(" This is a class dict ..........")
-#print(Cat.__dict__)
+my_wife = Lalita("Lalita",22,married)
+my_wife.behaviour()
